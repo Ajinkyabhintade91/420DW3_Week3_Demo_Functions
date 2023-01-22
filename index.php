5ec1d26e7c9345330b7b0ebbccd4cc377addfc16
Marc-Eric Boury <meboury@hotmail.com>
2023-01-22 11:52:48 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * 420DW3_Week3_Demo_Functions index.php
+ * 
+ * @author Marc-Eric Boury (Newironsides)
+ * @since 2023-01-22
+ * (c) Copyright 2023 Marc-Eric Boury 
+ */
